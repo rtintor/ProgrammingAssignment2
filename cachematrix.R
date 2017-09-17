086668f7599963a39056976597c72c2ae6d3de0e
Rodrigo Tintor <rodrigo.tintor@honeywell.com>
2017-09-16 22:53:09 -0500

    Mi cacheMatrix version for JHDSS
    Signed-off-by: Rodrigo Tintor <rodrigo.tintor@honeywell.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function that saves the inverse matrix from one matrix until it change.
+## If the matrix changes then save new matrix and new inverse amtrix
 
-## Write a short comment describing this function
+## Thi function save the inverse matrix from the last inverse process.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(minv) inv <<- minv
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function take the makeCahceMatrix and store the inverse, in the matrix is the same return the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x
+  mat <- x$getinv()
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data, ...)
+  x$setinv(mat)
+  mat
 }
